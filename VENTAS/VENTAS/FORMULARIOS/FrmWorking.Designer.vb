c05e1aaba0f26abbe993a5e79b6f38f1c946981e
Elvis <elvisvado316@gmail.com>
2017-08-08 16:40:30 -0600

    Modificaciones Formulario Working (Trabajando)

--- VENTAS/VENTAS/FORMULARIOS/FrmWorking.Designer.vb
+++ VENTAS/VENTAS/FORMULARIOS/FrmWorking.Designer.vb
@@ -25,11 +25,13 @@ Partial Class FrmWorking
         Me.btn_importar = New DevExpress.XtraEditors.SimpleButton()
         Me.barra_estado_importacion = New System.Windows.Forms.ProgressBar()
         Me.btn_salir = New DevExpress.XtraEditors.SimpleButton()
+        Me.cbo_opciones_importacion = New DevExpress.XtraEditors.ComboBoxEdit()
+        CType(Me.cbo_opciones_importacion.Properties, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'btn_importar
         '
-        Me.btn_importar.Location = New System.Drawing.Point(192, 47)
+        Me.btn_importar.Location = New System.Drawing.Point(200, 61)
         Me.btn_importar.Name = "btn_importar"
         Me.btn_importar.Size = New System.Drawing.Size(75, 23)
         Me.btn_importar.TabIndex = 0
@@ -37,34 +39,48 @@ Partial Class FrmWorking
         '
         'barra_estado_importacion
         '
-        Me.barra_estado_importacion.Location = New System.Drawing.Point(12, 24)
+        Me.barra_estado_importacion.Location = New System.Drawing.Point(12, 38)
+        Me.barra_estado_importacion.Maximum = 999999999
         Me.barra_estado_importacion.Name = "barra_estado_importacion"
         Me.barra_estado_importacion.Size = New System.Drawing.Size(437, 17)
         Me.barra_estado_importacion.TabIndex = 1
         '
         'btn_salir
         '
-        Me.btn_salir.Location = New System.Drawing.Point(192, 47)
+        Me.btn_salir.Location = New System.Drawing.Point(200, 61)
         Me.btn_salir.Name = "btn_salir"
         Me.btn_salir.Size = New System.Drawing.Size(75, 23)
         Me.btn_salir.TabIndex = 3
         Me.btn_salir.Text = "Salir"
         Me.btn_salir.Visible = False
         '
+        'cbo_opciones_importacion
+        '
+        Me.cbo_opciones_importacion.Location = New System.Drawing.Point(12, 12)
+        Me.cbo_opciones_importacion.Name = "cbo_opciones_importacion"
+        Me.cbo_opciones_importacion.Properties.Buttons.AddRange(New DevExpress.XtraEditors.Controls.EditorButton() {New DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)})
+        Me.cbo_opciones_importacion.Properties.Items.AddRange(New Object() {"Catalogo de Productos", "Historico de Ventas", "Historico de Importaciones"})
+        Me.cbo_opciones_importacion.Properties.NullValuePrompt = "Seleccionar Tipo"
+        Me.cbo_opciones_importacion.Properties.NullValuePromptShowForEmptyValue = True
+        Me.cbo_opciones_importacion.Size = New System.Drawing.Size(437, 20)
+        Me.cbo_opciones_importacion.TabIndex = 4
+        '
         'FrmWorking
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
-        Me.ClientSize = New System.Drawing.Size(461, 79)
+        Me.ClientSize = New System.Drawing.Size(461, 91)
+        Me.Controls.Add(Me.cbo_opciones_importacion)
         Me.Controls.Add(Me.btn_salir)
         Me.Controls.Add(Me.barra_estado_importacion)
         Me.Controls.Add(Me.btn_importar)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "FrmWorking"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        CType(Me.cbo_opciones_importacion.Properties, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -72,4 +88,5 @@ Partial Class FrmWorking
     Friend WithEvents btn_importar As DevExpress.XtraEditors.SimpleButton
     Friend WithEvents barra_estado_importacion As ProgressBar
     Friend WithEvents btn_salir As DevExpress.XtraEditors.SimpleButton
+    Friend WithEvents cbo_opciones_importacion As DevExpress.XtraEditors.ComboBoxEdit
 End Class
